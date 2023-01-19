49301377a9443dd764598f7c97967671364aa80d
Chandrika shekhar <96369349+chandrika017@users.noreply.github.com>
2023-01-19 15:59:24 +0600

    Initial commit
    Add files via upload

---
+++ Online_treatment/connection.php
@@ -0,0 +1,21 @@
+<?php
+$servername="localhost";
+$username="root";
+$password="";
+$dbname="registrationform";
+$conn=mysqli_connect($servername,$username,$password,$dbname);
+if($conn)
+{
+	
+
+	
+}
+else
+{
+	die("connection failed because".mysqli_connect_error());
+	
+}
+
+
+
+?>
